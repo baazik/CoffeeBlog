05403c88c1827cdbb058f17466034add41dd09d3
mbazant <mbazant@monetplus.cz>
2023-11-01 17:04:43 +0100

    basic setup of the project

---
+++ src/main/java/cz/hocuspocus/coffeeblog/CoffeeBlogApplication.java
@@ -0,0 +1,12 @@
+package cz.hocuspocus.coffeeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeBlogApplication.class, args);
+    }
+}
