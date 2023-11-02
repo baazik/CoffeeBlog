e928391323f9dfe6ed1ab809e1f20187ba9ddb92
mbazant <mbazant@monetplus.cz>
2023-11-02 15:41:58 +0100

    add application.properties with db config add jpa update pom.xml with jpa dependencies

--- src/main/java/cz/hocuspocus/coffeeblog/CoffeeBlogApplication.java
+++ src/main/java/cz/hocuspocus/coffeeblog/CoffeeBlogApplication.java
@@ -2,8 +2,10 @@ package cz.hocuspocus.coffeeblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CoffeeBlogApplication {
 
     public static void main(String[] args) {
