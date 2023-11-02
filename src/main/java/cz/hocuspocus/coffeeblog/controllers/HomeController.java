34594be933e18b0ec5e6f61fe493ae1e6be2ff9d
mbazant <mbazant@monetplus.cz>
2023-11-02 13:46:33 +0100

    adding basic pages of the web, improving of navigation

--- src/main/java/cz/hocuspocus/coffeeblog/controllers/HomeController.java
+++ src/main/java/cz/hocuspocus/coffeeblog/controllers/HomeController.java
@@ -9,5 +9,21 @@ public class HomeController {
     public String renderIndex() {
         return "pages/home/index";
     }
+
+    @GetMapping("/contact")
+    public String renderContact() {
+        return "pages/home/contact";
+    }
+
+    @GetMapping("/references")
+    public String renderReferences() {
+        return "pages/home/references";
+    }
+
+    @GetMapping("/skills")
+    public String renderSkills() {
+        return "pages/home/skills";
+    }
+
 }
 
