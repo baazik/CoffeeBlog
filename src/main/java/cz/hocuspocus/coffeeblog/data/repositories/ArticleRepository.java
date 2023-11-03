243307c7c50d7fc64bd3d0251f7cf735ce068077
mbazant <mbazant@monetplus.cz>
2023-11-03 10:51:24 +0100

    add ArticleEntity and ArticleRepository change port of DB to 3306

---
+++ src/main/java/cz/hocuspocus/coffeeblog/data/repositories/ArticleRepository.java
@@ -0,0 +1,10 @@
+package cz.hocuspocus.coffeeblog.data.repositories;
+
+import cz.hocuspocus.coffeeblog.data.entities.ArticleEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<ArticleEntity, Long> {
+
+
+
+}
